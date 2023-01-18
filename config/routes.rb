c3310ebaa14d51207ec311393fd46bc1e4700ee6
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-01-18 19:12:52 +0300

    Add the prescriptions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :prescriptions
-  resources :visits
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :visits do
+    resources :prescriptions
+  end
 end
