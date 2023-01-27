87032ce02b49ad74ff212f801423ed7c60f7bbba
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-01-27 18:06:49 +0300

    Remove unused code

--- spec/models/prescription_spec.rb
+++ spec/models/prescription_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Prescription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Prescription, type: :model do
+#   describe '#valid?' do
+#     it 'has a valid factory' do
+#       expect(build(:prescription)).to be_valid
+#     end
+#   end
+# end
