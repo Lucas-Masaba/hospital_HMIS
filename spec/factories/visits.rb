3583922ab8967b9849453c03e4dadaf768af5dbc
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-01-29 15:16:27 +0300

    Add character length for visit_category

--- spec/factories/visits.rb
+++ spec/factories/visits.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     visit_owner { Faker::Name.name }
     visit_type { Faker::Lorem.word }
     visit_date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
-    visit_category { Faker::Lorem.word }
+    visit_category { Faker::Lorem.word[3..10] }
     speciality { Faker::Lorem.word }
     member_no { Faker::Number.number(digits: 10) }
     service { Faker::Lorem.word }
