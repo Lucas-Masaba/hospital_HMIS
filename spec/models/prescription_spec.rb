3906522e8514dbe974ac90ab11ddbe6f9d9b310e
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-02-09 18:58:23 +0300

    Remove all commented out code

--- spec/models/prescription_spec.rb
+++ spec/models/prescription_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Prescription, type: :model do
     @prescription = build(:prescription , visit_id: @visit.id)
     @prescription.save
   end
-  
+
   describe '#valid?' do
     it 'has a valid factory' do
       expect(build(:prescription)).to be_valid
@@ -26,37 +26,7 @@ RSpec.describe Prescription, type: :model do
         expect(build(:prescription, product: 'a' * 51)).to_not be_valid
       end
     end
-
-    # context 'quantity validations' do
-    #   it 'is invalid without quantity' do
-    #     expect(build(:prescription, quantity: nil | '')).to_not be_valid
-    #   end
-    # end
-
-    # context 'dose validations' do
-    #   it 'is invalid without a dose' do
-    #     expect(build(:prescription, dose: nil | '')).to_not be_valid
-    #   end
-    # end
-
-    # context 'symbol validations' do
-    #   it 'is invalid without a symbol' do
-    #     expect(build(:prescription, symbol: nil | '')).to_not be_valid
-    #   end
-    # end
-
-    # context 'days validations' do
-    #   it 'is invalid without days' do
-    #     expect(build(:prescription, days: nil | '')).to_not be_valid
-    #   end
-    # end
-
-    # context 'stock validations' do
-    #   it 'is invalid without stock' do
-    #     expect(build(:prescription, stock: nil | '')).to_not be_valid
-    #   end
-    # end
-
+    
     context 'visit_id validations' do
       it 'is invalid without a visit_id' do
         expect(build(:prescription, visit_id: nil | '')).to_not be_valid
