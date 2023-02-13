7f65a6811ac9558f844f6c08824497f8e5ea8285
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-02-14 00:01:16 +0300

    Add validation tests for the patients

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Patient.new(name: 'Kemigisa Diana', age: 12, gender: "F", date_of_birth: "2023-01-23", address: 'Kampala', phone_number: +256777777777, next_of_kin: 'Mwine Tom', next_of_kin_phone: +256770773778, next_of_kin_address: 'Kampala') }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'age should be present' do
+    subject.age = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'gender should be present' do
+    subject.gender = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'date_of_birth should be present' do
+    subject.date_of_birth = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'address should be present' do
+    subject.address = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'next_of_kin should be present' do
+    subject.next_of_kin = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'next_of_kin_address should be present' do
+    subject.next_of_kin_address = nil
+    expect(subject).to_not be_valid
+  end
 end
