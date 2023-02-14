d01a9187cb93f0b94be7593c7ebc68452d6586fc
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-02-14 12:33:19 +0300

    Add validation tests for the prescriptions

--- spec/models/prescription_spec.rb
+++ spec/models/prescription_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Prescription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Prescription.new(product: 'Paracetamol', quantity: 10, symbol: '*', dose: 2, days: 3, stock: 10)
+  end
+
+  before { subject.save }
+
+  context 'product validations' do
+    it 'is invalid without a product' do
+      subject.product = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is invalid with a product that is too short' do
+      subject.product = 'aa'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is invalid with a product that is too long' do
+      subject.product = 'a' * 51
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'quantity validations' do
+    it 'is invalid without quantity' do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'symbol validations' do
+    it 'is invalid without a symbol' do
+      subject.symbol = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'dose validations' do
+    it 'is invalid without a dose' do
+      subject.dose = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'days validations' do
+    it 'is invalid without a days' do
+      subject.days = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'stock validations' do
+    it 'is invalid without a stock' do
+      subject.stock = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
