5de1be519ea3463a4f2a5a5f1240606a2d0b51dc
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-02-14 14:36:32 +0300

    Add validation tests for the visit_notes model

--- spec/models/visit_note_spec.rb
+++ spec/models/visit_note_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe VisitNote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    VisitNote.new(complaints: 'Pain in the abdomen', provisional_diagnosis: 'Gastritis')
+  end
+
+  before { subject.save }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is invalid without complaints' do
+    subject.complaints = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is invalid without a provisional_diagnosis' do
+    subject.provisional_diagnosis = nil
+    expect(subject).to_not be_valid
+  end
 end
