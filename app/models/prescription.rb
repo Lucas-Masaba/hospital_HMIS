caad65471e51bf7549f12f2cc52a21b944ea52b4
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-02-14 11:17:53 +0300

    Add validation to the prescription model

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,2 +1,8 @@
 class Prescription < ApplicationRecord
+  validates :product, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :symbol, presence: true
+  validates :dose, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :days, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :stock, presence: true
 end
