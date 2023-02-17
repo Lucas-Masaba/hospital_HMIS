41e8552b416047bae71d9e6e20a74417c120eeab
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-02-17 17:21:01 +0300

    Add validation tests for triage

--- spec/models/triage_spec.rb
+++ spec/models/triage_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Triage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Triage.new(name: 'Test Triage', value_one: 1, value_two: 2, date_time: DateTime.now)
+  end
+
+  before { subject.save }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is invalid without a value_one' do
+    subject.value_one = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is invalid without a value_two' do
+    subject.value_two = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is invalid without a date_time' do
+    subject.date_time = nil
+    expect(subject).to_not be_valid
+  end
 end
