f1e306e526f9a8144fb6b5196a208e23c3135d18
Lucas-Masaba <masabaluke@gmail.com>
2023-03-01 02:37:40 +0300

    Add relationships between triage and visits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_222546) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_231713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -136,6 +136,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_222546) do
     t.datetime "date_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "visit_id", null: false
+    t.index ["visit_id"], name: "index_triages_on_visit_id"
   end
 
   create_table "visit_notes", force: :cascade do |t|
@@ -162,5 +164,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_222546) do
 
   add_foreign_key "drugs", "visits"
   add_foreign_key "prescriptions", "visits"
+  add_foreign_key "triages", "visits"
   add_foreign_key "visits", "patients"
 end
