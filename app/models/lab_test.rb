d5d90a2721c6d7481fecbcef5640bfb2b69fa871
Lucas-Masaba <masabaluke@gmail.com>
2023-03-03 01:39:04 +0300

    Visit Lab Test Relationship

--- app/models/lab_test.rb
+++ app/models/lab_test.rb
@@ -1,6 +1,9 @@
 class LabTest < ApplicationRecord
+  belongs_to :visit, dependent: :destroy
+
   validates :name, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
   validates :status, presence: true
   validates :referral_status, presence: true
+  validates :visit_id, presence: true
 end
