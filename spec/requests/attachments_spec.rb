b509c54c6ed45469611cccdc4e25cbabde12d3e7
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2023-03-04 10:49:43 +0300

    Add visit_id to the tests

--- spec/requests/attachments_spec.rb
+++ spec/requests/attachments_spec.rb
@@ -1,11 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe '/attachments', type: :request do
+  let!(:patient) do
+    Patient.create!(
+      name: 'Kemigisa Diana',
+      age: 12, gender: 'F',
+      date_of_birth: '2023-01-23',
+      address: 'Kampala',
+      phone_number: 256_777_777_777,
+      next_of_kin: 'Mwine Tom',
+      next_of_kin_phone: 256_770_773_778,
+      next_of_kin_address: 'Kampala'
+    )
+  end
+
+  let!(:visit) do
+    Visit.create!(
+      visit_no: '2',
+      visit_owner: 'Caitylyn',
+      visit_date: '2023-01-23',
+      visit_type: 'review',
+      visit_category: 'insurance',
+      speciality: 'cardiology',
+      member_no: '2A5R2',
+      service: 'Consultation',
+      patient_id: patient.id
+    )
+  end
+
   let(:valid_attributes) do
     {
       name: 'Xray',
       description: 'Broken Femur',
-      admissions: 'None'
+      admissions: 'None',
+      visit_id: visit.id
     }
   end
 
@@ -77,7 +105,8 @@ RSpec.describe '/attachments', type: :request do
         {
           name: 'CT Scan',
           description: 'Tumor',
-          admissions: 'None'
+          admissions: 'None',
+          visit_id: visit.id
         }
       end
 
