9e19c52948ff70f1deae41df926c290a6233bd56
Lucas-Masaba <masabaluke@gmail.com>
2023-03-14 18:56:56 +0300

    Fix relationship

--- app/models/triage.rb
+++ app/models/triage.rb
@@ -1,4 +1,6 @@
 class Triage < ApplicationRecord
+  belongs_to :visit
+
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :value_one, presence: true
   validates :value_two, presence: true
