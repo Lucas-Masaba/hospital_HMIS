d2081dddf4f46c4d08dabbf1934f74e0c1e9e51d
Lucas-Masaba <masabaluke@gmail.com>
2023-03-15 18:30:25 +0300

    Add foreign key to attachments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_213519) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_070340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -168,6 +168,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_213519) do
     t.index ["patient_id"], name: "index_visits_on_patient_id"
   end
 
+  add_foreign_key "attachments", "visits"
   add_foreign_key "drugs", "visits"
   add_foreign_key "lab_tests", "visits"
   add_foreign_key "prescriptions", "visits"
